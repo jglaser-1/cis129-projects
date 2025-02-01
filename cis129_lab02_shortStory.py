f8fae5773483c08db91f156e6bd91b058c946545
jglaser-1 <jglaser1@mail.pima.edu>
2025-02-01 00:20:59 -0700

    Update cis129_lab02_shortStory.py
    Adds author details
    Adds short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -3,5 +3,5 @@
 # Prints a short story
 
 Print('Another night of rushing to finish every assignment before 11:59.')
-Print('You submit everything and start working on the final assignment that's due.')
+Print('You submit everything and start working on the final assignment that is due.')
 Print('Then when you open it, you realize why you were given a month to complete it...')
