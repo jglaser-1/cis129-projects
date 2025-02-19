3ea4a16cf33771fd81a1e56f0a92e4a0159d1c0b
jglaser-1 <jglaser1@mail.pima.edu>
2025-02-18 19:45:20 -0700

    Update cis129_lab03_coffeeShop.py
    Adds additional menu items to program.

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -8,14 +8,22 @@ print("Number of coffees bought?")
 coffeesBought = int(input())
 print("\nNumber of muffins bought?")
 muffinsBought = int(input())
+print("\nNumber of cookies bought?")
+cookiesBought = int(input())
+print("\nNumber of sandwiches bought?")
+sandwichesBought = int(input())
 print("***************************************\n\n***************************************")
 
 #Declaring variables to calculate the total cost
 costPerCoffee = 5.00
 costPerMuffin = 4.00
+costPerCookie = 3.50
+costPerSandwich = 6.50
 coffeeTotalCost = coffeesBought * costPerCoffee
 muffinTotalCost = muffinsBought * costPerMuffin
-subtotal = coffeeTotalCost + muffinTotalCost
+cookieTotalCost = cookiesBought * costPerCookie
+sandwichTotalCost = sandwichesBought * costPerSandwich
+subtotal = coffeeTotalCost + muffinTotalCost + cookieTotalCost + sandwichTotalCost
 tax = subtotal * 0.06
 taxTotal = subtotal * 1.06
 
@@ -25,12 +33,21 @@ print("My Coffee and Muffin Shop Receipt")
 if coffeesBought > 1:
     print(str(coffeesBought) + " Coffees at $5 each: $" + f"{coffeeTotalCost:.2f}")
 else:
-    print(str(coffeesBought) + " Coffee at $5 each: $" + f"{coffeeTotalCost:.2f}")
+    print(str(coffeesBought) + " Coffee: $" + f"{coffeeTotalCost:.2f}")
 if muffinsBought > 1:
     print(str(muffinsBought) + " Muffins at $4 each: $" + f"{muffinTotalCost:.2f}")
 else:
-    print(str(muffinsBought) + " Muffin at $4 each: $" + f"{muffinTotalCost:.2f}")
+    print(str(muffinsBought) + " Muffin: $" + f"{muffinTotalCost:.2f}")
+if cookiesBought > 1:
+    print(str(cookiesBought) + " Cookies at $3.50 each: $" + f"{cookieTotalCost:.2f}")
+else:
+    print(str(cookiesBought) + " Cookie: $" + f"{cookieTotalCost:.2f}")
+if sandwichesBought > 1:
+    print(str(sandwichesBought) + " Sandwiches at $6.50 each: $" + f"{SandwichTotalCost:.2f}")
+else:
+    print(str(sandwichesBought) + " Sandwiche: $" + f"{SandwichTotalCost:.2f}")
 print("6% tax: $" + f"{tax:.2f}")
 print("---------")
 print("Total: $" + f"{taxTotal:.2f}")
-print("***************************************")
+print("\n\nThank you for coming!")
+print("\n***************************************")
